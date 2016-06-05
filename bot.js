5764c291e61d9d4203f6c4d3ffbff5c0746ef7a5
Francesco Mondora <francesco.mondora@mondora.com>
2016-06-05 20:46:53 +0200

    Aggiunte previsioni

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 
 
-var token = '234912877:AAGtmlM3JCXrRcQZbJx9AUuVBcz5DxHuwdM';
 
 var Bot = require('node-telegram-bot-api'),
     bot = new Bot(token, { polling: true });
@@ -20,6 +19,12 @@ bot.onText(/^\/sum((\s+\d+)+)$/, function (msg, match) {
   bot.sendMessage(msg.chat.id, result).then(function () {
     // reply sent!
   });
+});
+
+bot.onText(/^weather/, function( msg, match) {
+  result = "https://my.meteoblue.com/visimage/meteogram_multiSimple?apikey=5838a18e295d&cache=no&city=Berbenno+di+Valtellina&iso2=it&lat=46.166401&lon=9.747340&asl=370&tz=Europe%2FRome&dt=1&fcstlength=72&look=CELSIUS%2CKILOMETER_PER_HOUR&lang=en&ts=1465152082&sig=4e515a75fea741080e179b716bef8da8";
+  bot.sendMessage(msg.chat.id, result).then(function(){}) ;
+
 });
 
 
